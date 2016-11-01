34373d8788eb778a863b6d4182af8b7380fe2481
Blake Geraci <bgeraci851@hcc.edu>
2016-11-01 04:21:49 -0400

    Add names to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # creating a before filter/action that is only run if the devise_controller is being used
+  # this includes
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end
